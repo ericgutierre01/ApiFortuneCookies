5b5727b034ca8fb9384fa8542958a3e90453f7a0
ericgutierre01 <58093470+ericgutierre01@users.noreply.github.com>
2024-03-01 14:53:57 -0400

    init
    init

---
+++ GalletaFortunaApi/Program.cs
@@ -0,0 +1,48 @@
+﻿using GalletaFortunaApi;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllHeaders",
+    builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseCors("AllowAllHeaders");
+
+app.MapGet("/getFortune", () =>
+{
+    var file = File.ReadAllText(Directory.GetCurrentDirectory() + "/fortune.json");
+    var galletas = (List<galleta>)JsonConvert.DeserializeObject(file, typeof(List<galleta>));
+    var randon = Random.Shared.Next(0, galletas.Count);
+    var galleta = galletas.SingleOrDefault(g => g.id == randon);
+    return galleta;
+})
+.WithName("getFortune");
+
+app.Run();
+
---
+++ GalletaFortunaApi/galleta.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace GalletaFortunaApi
+{
+    public class galleta
+    {
+        public int id { get; set; }
+        public string mensaje { get; set; }
+    }
+}
+
